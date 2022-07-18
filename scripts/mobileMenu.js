921eb1aff5ae0515044630551dbc388db7bbe46a
tinayetsina <tinayetsina@outlook.com>
2022-07-19 00:46:15 +0800

    update mobileMenu.js - resolved linter errors

--- scripts/mobileMenu.js
+++ scripts/mobileMenu.js
@@ -3,13 +3,11 @@ const navMenu = document.querySelector('.mobile-menu');
 const xBox = document.querySelector('.header-cross');
 const blurBackground = document.querySelector('.headline');
 
-
 // Event Listener for hamburger menu
 hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('active');
   blurBackground.classList.toggle('active');
 });
-
 
 // Event Listener for xBox on click
 xBox.addEventListener('click', () => {
