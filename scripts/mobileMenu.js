a7db4e8474020beb6746f59624fa6c1fd98817cc
tinayetsina <tinayetsina@outlook.com>
2022-07-20 00:41:14 +0800

    added dataObject to put data

--- scripts/mobileMenu.js
+++ scripts/mobileMenu.js
@@ -9,10 +9,88 @@ const openModalButtons = document.querySelectorAll('[data-modal-target]');
 const closeModalButtons = document.querySelectorAll('[data-closed-button]');
 const overlay = document.getElementById('overlay');
 
+// Modal HTML Text Targets
+
+let modalHeader = document.querySelector('.modal-header-text');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DOM Data from Javascript
+
+let modalObject = {
+  modal_1: {
+    portHeading: 'Tonic Objec',
+    portList1: 'CANOPY',
+    portList2: 'Back End Dev',
+    portList3: '2015',
+    toolList1: 'HTML',
+    toolList2: 'CSS',
+    toolList3: 'Javascript',
+    paragraph: 'lorem ipsum blah blah',
+    backGround: 'img/xx.png'
+  },
+  modal_2: {
+    portHeading: 'Mulit-Post Stories',
+    portList1: 'Facebook',
+    portList2: 'Full Stack Dev',
+    portList3: '2015',
+    toolList1: 'HTML',
+    toolList2: 'Ruby on rails',
+    toolList3: 'Javascript',
+    paragraph: 'lorem ipsum blah blah',
+    backGround: 'img/xx.png'
+  },
+  modal_3: {
+    portHeading: 'Facebook 360',
+    portList1: 'Facebook',
+    portList2: 'Full Stack Dev',
+    portList3: '2015',
+    toolList1: 'HTML',
+    toolList2: 'Python',
+    toolList3: 'Mongo DB',
+    paragraph: 'lorem ipsum blah blah',
+    backGround: 'img/xx.png'
+  },
+  modal_4: {
+    portHeading: 'Uber Navigation',
+    portList1: 'UBER',
+    portList2: 'Lead Developer',
+    portList3: '2018',
+    toolList1: 'HTML',
+    toolList2: 'CSS',
+    toolList3: 'Javascript',
+    paragraph: 'lorem ipsum blah blah',
+    backGround: 'img/xx.png'
+  }
+}
+
 openModalButtons.forEach( button => {
   button.addEventListener('click', () => {
+
+    modalHeader.textContent= modalObject.modal_1.portHeading;
+
     modal.classList.add('active');
-    overlay.classList.add('active');  
+    overlay.classList.add('active');
   });
 });
 
@@ -22,22 +100,6 @@ closeModalButtons.forEach( button => {
     overlay.classList.remove('active');
   });
 });
-
-function openModal(modal) {
-  if (modal == null) {
-    return
-  }
-  modal.classList.add('active');
-  overlay.classList.add('active');
-}
-
-function openModal(modal) {
-  if (modal == null) {
-    return
-  }
-  modal.classList.remove('active');
-  overlay.classList.remove('active');
-}
 
 
 
