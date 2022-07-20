d11dd418e0cd53cbb44c96f03a5f165cc136f171
tinayetsina <tinayetsina@outlook.com>
2022-07-20 20:53:28 +0800

    update - resolved contact form

--- scripts/contactForm.js
+++ scripts/contactForm.js
@@ -1,12 +1,23 @@
+// Global defines
+const ERROR_MESSAGE = 'Please enter lowecase only !';
+
 const formName = document.getElementById('form-name');
 const formEmail = document.getElementById('form-email');
 const formMessage = document.getElementById('form-message');
 const formButton = document.getElementById('form-button');
+const formError = document.getElementById('form-error');
 
 formButton.addEventListener('click', function(e) {
 
-    e.preventDefault();
-    console.log(formNameChecker(formEmail));
+    
+
+    if (!formNameChecker(formEmail)) {
+        e.preventDefault();
+        formError.textContent = "Lower case --> "+formEmail.value.toLowerCase();
+        console.log(formNameChecker(formEmail));
+    }
+
+
 });
 
 function formNameChecker(formName) {
