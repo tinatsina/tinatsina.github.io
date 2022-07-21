75906795c38bfdf5507ce9245228544d6ece48ea
tinayetsina <tinayetsina@outlook.com>
2022-07-21 10:26:20 +0800

    update - resolved linter errors

--- scripts/contactForm.js
+++ scripts/contactForm.js
@@ -1,26 +1,23 @@
-const formName = document.getElementById('form-name');
 const formEmail = document.getElementById('form-email');
-const formMessage = document.getElementById('form-message');
 const formButton = document.getElementById('form-button');
 const formError = document.getElementById('form-error');
 
-// Added event listener to form
-formButton.addEventListener('click', function(e) {
-    if (!formNameChecker(formEmail)) {
-        e.preventDefault();
-        formError.textContent = "Lower case --> "+formEmail.value.toLowerCase();
-        console.log(formNameChecker(formEmail));
-    }
-});
-
 // Function to check validity of email name
 function formNameChecker(formName) {
-    let result = false
-    const formNameLowerCase = formName.value.toLowerCase();
-    if (formName.value === formNameLowerCase) {
-        result = true;
-    } else {
-        result = false;
-    }
-    return result;
+  let result = false;
+  const formNameLowerCase = formName.value.toLowerCase();
+  if (formName.value === formNameLowerCase) {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result;
 }
+
+// Added event listener to form
+formButton.addEventListener('click', (e) => {
+  if (!formNameChecker(formEmail)) {
+    e.preventDefault();
+    formError.textContent = `Lower case --> ${formEmail.value.toLowerCase()}`;
+  }
+});
