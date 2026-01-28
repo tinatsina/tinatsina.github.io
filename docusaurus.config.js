49d295369bc4d547871185a72b82fea66d28486c
Tinaye Tsinakwadi <tinayetsina@outlook.com>
2026-01-28 13:29:32 +0100

    update - changed to sysadmin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Tinaye",
-  tagline: "Data Engineer",
+  title: "Tinaye H Tsinakwadi",
+  tagline: "System Administrator and Linux Developer",
   favicon: "img/dragon.ico",
 
   // Set the production url of your site here
